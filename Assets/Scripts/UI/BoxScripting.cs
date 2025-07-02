97013ef9fb47a6b3671b720db530f6df6d2f74e8
Mehul Parakh <mehul@underpinservices.com>
2025-07-02 12:51:08 +0530

    FIX: Code refactor + removed csvExporter script

--- Assets/Scripts/UI/BoxScripting.cs
+++ Assets/Scripts/UI/BoxScripting.cs
@@ -3,21 +3,21 @@ using UnityEngine.UI;
 
 public class BoxScripting : MonoBehaviour
 {
-    internal bool isAnim = false;
-    [SerializeField] private Image BoxBg_Image;
-    [SerializeField] private GameObject BoxBg_Object;
+  internal bool isAnim = false;
+  [SerializeField] private Image BoxBg_Image;
+  [SerializeField] private GameObject BoxBg_Object;
 
-    internal void SetBG(Sprite bg)
+  internal void SetBG(Sprite bg)
+  {
+    if (isAnim)
     {
-        if (isAnim)
-        {
-            if (BoxBg_Image) BoxBg_Image.sprite = bg;
-            if (BoxBg_Object) BoxBg_Object.SetActive(true);
-        }
+      if (BoxBg_Image) BoxBg_Image.sprite = bg;
+      if (BoxBg_Object) BoxBg_Object.SetActive(true);
     }
+  }
 
-    internal void ResetBG()
-    {
-        if (BoxBg_Object) BoxBg_Object.SetActive(false);
-    }
+  internal void ResetBG()
+  {
+    if (BoxBg_Object) BoxBg_Object.SetActive(false);
+  }
 }
